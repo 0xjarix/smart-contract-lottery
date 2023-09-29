4fcc495f0894daeb7ae2e6d0c1def2140fe024c7
joseph abou rizk <joseph.abou-rizk@epita.fr>
2023-09-29 08:07:43 +0200

    events & errors

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -30,17 +30,49 @@ pragma solidity ^0.8.18;
  */
 
 contract Raffle {
+    // custom error messages
+    error Raffle__NotEnoughEthSent();
+    error Raffle__NotEnoughTimePassed();
+
+    // state variables
     uint256 private immutable i_entranceFee;
+    // @dev Duration of lottery in seconds
+    uint256 private immutable i_interval;
+    address payable[] private s_players;
+    uint256 private s_rafflePot;
+    uint256 private s_lastTimeStamp;
 
-    constructor(uint256 entranceFee) {
+    //events
+    event EnteredRaffle(address indexed player);
+
+    //constructor
+    constructor(uint256 entranceFee, uint256 interval) {
         i_entranceFee = entranceFee;
+        i_interval = interval
+        s.lastTimeStamp = block.timestamp;
     }
 
-    function enterRaffle() public payable {
-        require(msg.value > entranceFee, "Not enough ETH sent");
+    //functions
+    function enterRaffle() external payable {
+        // require(msg.value >= i_entranceFee, "Not enough ETH sent"); more traditional yet less optimized way of doing it
+        if (msg.value < i_entranceFee) {
+            revert Raffle__NotEnoughEthSent();
+        }
+        s_players.push(payable(msg.sender));
+        // Makes migration easier
+        // Makes front-end "indexing" easier
+        emit EnteredRaffle(msg.sender);
+        s_rafflePot += msg.value;
     }
 
-    function pickWinner() public {}
+    // 1. Get a random number
+    // 2. Use the random number to pick a player
+    // 3. Be automatically called
+    function pickWinner() external {
+        if (block.timestamp - last.timestamp <= i_interval)
+            revert Raffle__NotEnoughTimePassed();
+        
+    }
 
     // Getters
     function getEntranceFee() public view returns (uint256) {
